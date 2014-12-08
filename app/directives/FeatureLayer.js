1e2da30892bfa2b902c51a326621e32d11d13dee
Steven Birkner <stevebirk36@gmail.com>
2014-12-08 15:05:52 -0500

    init

---
+++ app/directives/FeatureLayer.js
@@ -0,0 +1,71 @@
+define([
+  'app',
+  'esri/layers/FeatureLayer',
+  'esri/layers/ArcGISDynamicMapServiceLayer',
+  "dojo/dom",
+  "dojo/on",
+  "dojo/query",
+  "dojo/domReady!"
+], function (app, FeatureLayer, ArcGISDynamicMapServiceLayer, dom, on, query) {
+  app.directive('esriFeatureLayer', function(){
+    // this object will tell angular how our directive behaves
+    return {
+      // only allow esriFeatureLayer to be used as an element (<esri-feature-layer>)
+      restrict: 'E',
+
+      // require the esriFeatureLayer to have its own controller as well an esriMap controller
+      // you can access these controllers in the link function
+      require: ["esriFeatureLayer", "^esriMap"],
+
+      // replace this element with our template.
+      // since we aren't declaring a template this essentially destroys the element
+      replace: true,
+
+      // define an interface for working with this directive
+      controller: function($scope, $element, $attrs){
+
+        // now is a good time to declare our FeautreLayer
+        var layer = new ArcGISDynamicMapServiceLayer($attrs.url,
+          {
+            id: "layer",
+            visible: true,
+            opacity: 0.5,
+          });
+        //default layer ids
+        var visibleLayerIDs = [0,1,2,4];
+        $scope.visLayer = 4;
+        layer.setVisibleLayers(visibleLayerIDs);
+        
+        on(dom.byId('layerSelect'), 'change', function(evt) {
+          //console.log(evt);
+          if (evt.target.value === '4') {
+            visibleLayerIDs = [0,1,2,4];
+            layer.setVisibleLayers(visibleLayerIDs);
+            $scope.visLayer = 4;
+
+          } else {
+            visibleLayerIDs = [0,1,2,3];
+            layer.setVisibleLayers(visibleLayerIDs);
+            $scope.visLayer = 3;
+          }
+          
+        });
+        // lets expose a function to get the layer
+        this.getLayer = function(){
+          return layer;
+        };
+      },
+
+      // now we can link our directive to the scope, but we can also add it to the map..
+      link: function(scope, element, attrs, controllers){
+        // controllers is now an array of the controllers from the 'require' option
+        var layerController = controllers[0];
+        var mapController = controllers[1];
+
+        // now we can use the 'addLayer' method exposed on the controller
+        // of the esriMap directive to add the layer to the map
+        mapController.addLayer(layerController.getLayer());
+      }
+    };
+  });
+});
